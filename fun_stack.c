6282d58f2742e683367427391e2bd055dbd1fb8d
Michal Kwiatkowski <01169586@pw.edu.pl>
2022-01-03 23:11:07 +0100

    dodano działającą sterte w pliku fun_stack.c

--- fun_stack.c
+++ fun_stack.c
@@ -4,23 +4,48 @@
 
 #include "fun_stack.h"
 
+typedef struct lista {
+        char* funame;
+        int par_level;
+        struct lista* prev_stack;
+} sterta_t;
 
-int top_of_funstack(void){
 
+static sterta_t *sterta;
+char funame_tmp[256]; //maksymalna długość nazwy funkcji
 
+void stack_init( sterta_t* sterta ){
+        sterta = malloc( sizeof sterta );
+        sterta->funame = malloc(sizeof (char));
+        sterta->funame = '\0';
+        sterta->par_level = 0;
+        sterta->prev_stack = NULL;
 }
 
-
-void put_on_fun_stack(int par_level, char *funname){
-
-
+int top_of_funstack(){
+        return sterta->par_level;
 }
 
-
-char *get_from_fun_stack(void){
-
-
+void put_on_fun_stack (int par_level, char *funame){
+        sterta_t *tmp = malloc( sizeof tmp);
+        tmp->funame = malloc(sizeof tmp->funame * strlen(funame) + 1);
+        strcpy( tmp->funame, funame);
+        tmp->par_level = par_level;
+        tmp->prev_stack = sterta;
+        sterta = tmp;
 }
+
+char* get_from_fun_stack(){
+        strcpy( funame_tmp, sterta->funame);
+
+        sterta_t * to_remove = sterta;
+        sterta = sterta->prev_stack;
+        free(to_remove->funame);
+        free(to_remove);
+
+        return funame_tmp;
+}
+
 
 
 void add_zapamietane_funkcje(char *funame, list_zapamietane_funkcje lista){
